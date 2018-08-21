bd2af9aa11b449870c9df0c78e414833615b12dd
JaimeNufio <jaimeenufio@gmail.com>
2018-08-20 21:41:19 -0400

    Slider Working, Fixed "must be connected" bug

--- app/src/main/java/com/jaimenufio/blu/MainActivity.java
+++ app/src/main/java/com/jaimenufio/blu/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -29,25 +30,40 @@ public class MainActivity extends AppCompatActivity {
 
     Button forward_btn, forward_left_btn, forward_right_btn, reverse_btn, bluetooth_connect_btn, alpha_btn, beta_btn;
     ToggleButton on_switch;
-    TextView tBox;
+    SeekBar scroll;
+    TextView tBox,sBox, scrollText;
 
     byte command; //string variable that will store value to be transmitted to the bluetooth module
     ArrayList<Byte> down = new ArrayList<Byte>();
 
-    protected  void updateList(Byte command){
+    protected void updateList(Byte command) {
         String t = "";
         down.add(command);
-        for (Byte s: down){
-            t+=""+s;
+        for (Byte s : down) {
+            t += "" + s;
         }
         tBox.setText(t);
     }
-    protected  void updateList(){
+
+    protected void updateList() {
         String t = "";
-        for (Byte s: down){
-            t+=""+s;
+        for (Byte s : down) {
+            t += "" + s;
         }
         tBox.setText(t);
+    }
+
+    protected void sendLinear(int num) {
+
+        try{
+            byte x = (byte)(Math.floor(num/2)+50);
+            if (on_switch.isChecked()) {
+                outputStream.write((byte) x);
+            }
+            sBox.setText(""+x);
+        }catch(IOException e){
+
+        }
     }
 
 
@@ -66,66 +82,82 @@ public class MainActivity extends AppCompatActivity {
         tBox = (TextView) findViewById(R.id.tBox);
         alpha_btn = (Button) findViewById(R.id.alpha);
         beta_btn = (Button) findViewById(R.id.beta);
+        scroll = (SeekBar) findViewById(R.id.scroll);
+        scrollText = (TextView) findViewById(R.id.scrollText);
+        sBox = (TextView) findViewById(R.id.tBox1);
 
 
-        //OnTouchListener code for the forward button (button long press)
+        scroll.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int t;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                t=progress;
+                command=(byte)t;
+                scrollText.setText(""+t);
+                sendLinear(t);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                sBox.setText("");
+            }
+        });
+
         forward_btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
-                if (event.getAction() == MotionEvent.ACTION_DOWN && on_switch.isChecked()) //MotionEvent.ACTION_DOWN is when you hold a button down
+                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
                 {
                     command = 1;
                     updateList(command);
 
-                    try
-                    {
-                        outputStream.write(command); //transmits the value of command to the bluetooth module
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
-                {
+                } else if (event.getAction() == MotionEvent.ACTION_UP) { //MotionEvent.ACTION_UP is when you release a button
                     Byte t = 1;
                     down.remove(t);
                     command = 126;
                     updateList();
-                    try
-                    {
-                        outputStream.write(command);
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-
                 }
 
                 return false;
-            }
 
-        });
+            }});
 
         //OnTouchListener code for the reverse button (button long press)
         reverse_btn.setOnTouchListener(new View.OnTouchListener(){
             @Override
             public boolean onTouch(View v, MotionEvent event)
             {
-                if(event.getAction() == MotionEvent.ACTION_DOWN && on_switch.isChecked())
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
                 {
                     command = 2;
                     updateList(command);
 
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
                 else if(event.getAction() == MotionEvent.ACTION_UP)
@@ -134,13 +166,12 @@ public class MainActivity extends AppCompatActivity {
                     down.remove(t);
                     command = 125;
                     updateList();
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch(IOException e)
-                    {
-
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                 }
@@ -153,18 +184,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event)
             {
-                if(event.getAction() == MotionEvent.ACTION_DOWN && on_switch.isChecked())
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
                 {
                     command = 3;
                     updateList(command);
 
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
                 else if(event.getAction() == MotionEvent.ACTION_UP)
@@ -173,13 +203,12 @@ public class MainActivity extends AppCompatActivity {
                     down.remove(t);
                     command = 124;
                     updateList();
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch(IOException e)
-                    {
-
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                 }
@@ -192,18 +221,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event)
             {
-                if(event.getAction() == MotionEvent.ACTION_DOWN && on_switch.isChecked())
+                if(event.getAction() == MotionEvent.ACTION_DOWN )
                 {
                     command = 4;
                     updateList(command);
 
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
                 else if(event.getAction() == MotionEvent.ACTION_UP)
@@ -212,13 +240,12 @@ public class MainActivity extends AppCompatActivity {
                     down.remove(t);
                     command = 123;
                     updateList();
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                 }
@@ -230,18 +257,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event)
             {
-                if(event.getAction() == MotionEvent.ACTION_DOWN && on_switch.isChecked())
+                if(event.getAction() == MotionEvent.ACTION_DOWN )
                 {
                     command = 5;
                     updateList(command);
 
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
                 else if(event.getAction() == MotionEvent.ACTION_UP)
@@ -250,13 +276,12 @@ public class MainActivity extends AppCompatActivity {
                     down.remove(t);
                     command = 122;
                     updateList();
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                 }
@@ -268,18 +293,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event)
             {
-                if(event.getAction() == MotionEvent.ACTION_DOWN && on_switch.isChecked())
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
                 {
                     command = 6;
                     updateList(command);
 
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
                 else if(event.getAction() == MotionEvent.ACTION_UP)
@@ -288,19 +312,20 @@ public class MainActivity extends AppCompatActivity {
                     down.remove(t);
                     command = 121;
                     updateList();
-                    try
-                    {
-                        outputStream.write(command);
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
+                    if (on_switch.isChecked()) {
+                        try {
+                            outputStream.write(command); //transmits the value of command to the bluetooth module
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                 }
                 return false;
             }
         });
+
+
 
         //Button that connects the device to the bluetooth module when pressed
         bluetooth_connect_btn.setOnClickListener(new View.OnClickListener() {
